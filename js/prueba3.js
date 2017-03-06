83c1fdb4c5be3107b76ce6b02321859631a82ad5
David Rubio <David Rubio>
2017-03-06 15:01:46 +0100

    ejemplo select

--- js/prueba3.js
+++ js/prueba3.js
@@ -1,11 +1,18 @@
 var app=angular.module('universidad-app',[]);
 app.controller('profesorCtrl', function($scope){
     $scope.datos=["David Rubio","Sergio Rubio","Sergio Javier Rubio"];
-    
+    $scope.pais=2;
     $scope.datos2= [{nombre:"David Rubio",edad:39,ciudad:"Zaragoza"},
     {nombre:"Sergio Rubio",edad:32,ciudad:"Zaragoza"},
     {nombre:"Sergio Javier Rubio",edad:5,ciudad:"Zaragoza"}
     
-    ]
+];
 
+    $scope.paises =
+    [
+        {id:1,nombre:"España"},
+        {id:2,nombre:"Irlanda"},
+        {id:3,nombre:"EEUU"}
+    ]
+  ;
 });
