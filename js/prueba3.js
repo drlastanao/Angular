23813615c2d73c3510c08511cf4897e8a6e077ba
David Rubio <David Rubio>
2017-03-06 08:34:04 +0100

    repeat con datos

--- js/prueba3.js
+++ js/prueba3.js
@@ -2,5 +2,10 @@ var app=angular.module('universidad-app',[]);
 app.controller('profesorCtrl', function($scope){
     $scope.datos=["David Rubio","Sergio Rubio","Sergio Javier Rubio"];
     
+    $scope.datos2= [{nombre:"David Rubio",edad:39,ciudad:"Zaragoza"},
+    {nombre:"Sergio Rubio",edad:32,ciudad:"Zaragoza"},
+    {nombre:"Sergio Javier Rubio",edad:5,ciudad:"Zaragoza"}
+    
+    ]
 
 });
